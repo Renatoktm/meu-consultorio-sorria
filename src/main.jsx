d2b8cc39010a55dcda4b72e9a7981dfc1eba0fcf
Renatoktm <tcrp2902@gmail.com>
2026-05-14 09:30:13 -0300

    Corrige client id do Google Calendar

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './styles/global.css'
 import App from './App.jsx'
 
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  '744463585572-k016407m4248pg7p43l16846k3shpi3p.apps.googleusercontent.com'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ''}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
